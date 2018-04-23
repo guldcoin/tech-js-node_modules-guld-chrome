0a6fae9b43ca94e16430910144ca1f65ae2a2c6e
Ira Miller <public@iramiller.com>
2018-04-23 15:01:25 -0500

    basic API framework and generate button

--- js/popup.js
+++ js/popup.js
@@ -7,12 +7,14 @@ function loadLogin() {
     return `<option value="${fpr}">${fpr}</option>`
   });
   var keyopts = keymap.join("\n");
-  wrapper.innerHTML = `<form id="key-login-form">
+  wrapper.innerHTML = `<button id="goto-generate-button" value="Generate">Generate</button><br>
+  <form id="key-login-form">
   <select id="key-fpr">${keyopts}</select>
   <input id="login-passphrase" type="password"></input>
   <input id="login-submit" type="submit" value="Login"></input>
   </form>`
   document.getElementById("key-login-form").addEventListener("submit", submitLogin);
+  document.getElementById("goto-generate-button").addEventListener("click", loadGenerate);
 }
 
 function submitLogin() {
